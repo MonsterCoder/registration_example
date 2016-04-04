9ced92ec32e0045b6e6299882394881aea0de897
monstercoder <monstercoder@gmail.com>
2016-04-03 21:22:50 -0500

    email process

--- email_service.js
+++ email_service.js
@@ -25,15 +25,18 @@ EmailService.prototype.start = function() {
 
       console.log("[AMQP] connected at "+ config.rabbit_url);
       self.conn = conn;
-      self.emit("[AMQP] connect", conn);
-  	})
+      self.emit("[AMQP] connected", conn);
+  	});
+
+  	return self;
 }
 
 EmailService.prototype.send_welcome_email = function(customer) {
     var self = this;
+
     if (self.ch) {
      	process.nextTick(function() {
-     			send_to_queue(self.ch, email_queue, customer.email)
+     			send_to_queue(self.ch, email_queue, customer.email);
      	})
     } else {
     	self.conn.createConfirmChannel( function(err, ch) {
@@ -43,16 +46,27 @@ EmailService.prototype.send_welcome_email = function(customer) {
 			self.ch = ch;
 			ch.on("error", function(err) { console.error(err)})
     	
-        
-        	ch.publish('', email_queue, customer.email, { persistent: true },
-		    	function (err, ok) {
-		    		if (err) {
-		    			console.error("[AMQP] publish ", err);
-		        		pubChannel.connection.close();
-		    		}
-		    })
+          
+            send_to_queue(self.ch, email_queue, customer.email)
       })
     }
 
 }
+
+function send_to_queue(ch, q_name, content) {
+    ch.publish('', q_name, new Buffer(content), { persistent: true },
+    	function (err, ok) {
+    		if (err) {
+    			console.error("[AMQP] publish ", err);
+        		pubChannel.connection.close();
+    		}
+    })
+}
+
+var service = new EmailService().start();
+
+module.exports = service;
+
+
+
 
